80e08972e4b52c08e8d658f751ed2f9a8fe13dd6
manojkgorle <gorlemanojk@gmail.com>
2024-12-02 07:12:28 +0000

    assets, readme, acceptance

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 Light client for Kasavinal DA
 
+DA E2E flow
+![DA E2E flow](./assets/daprocess.png)
+
 Nodes in Kasavinal DA are differentied as Light and full nodes.
 ![DA Nodes](./assets/da.png)
 
@@ -9,3 +12,27 @@ Full nodes participate in consensus, produce blocks, store data. While Light nod
 
 If any non consensus full node participating network, detects that data received from consensus full nodes to light clients is byzantine, it creates and sends fraud proofs to all light clients connected to that full node in the network.
 ![Fraud Proofs](./assets/fp.png)
+
+## How to run and testing:
+
+```go
+    git clone git@github.com:manojkgorle/hypersdk.git
+    git clone git@github.com:manojkgorle/kasavinal.git
+    cd hypersdk
+    git checkout da
+    cd examples/morpheusvm
+    ./scripts/build.sh
+    ./scripts/run.sh
+
+    // In new shell with morepheusvm as roo
+    ./build/morpheus-cli chain import-anr
+    ./build/morpheus-cli key import ed25519 demo.pk
+    ./build/morpheus-cli spam run
+    // choose spam options
+
+    // In new shell with kasavinal as root.
+    // copy the rpc uri and chain id str after running the command import-anr in last shell
+    // replace the chain id and uri fields in main.go
+    go run main.go
+
+```
--- main.go
+++ main.go
@@ -16,9 +16,9 @@ import (
 )
 
 const (
-	url        = "http://127.0.0.1:45567/ext/bc/2mgSrj2xG9gagfhi5vSJwe6XiH2HmWinPxYCxhoZGSQzynRWPG"
+	url        = "http://127.0.0.1:41379/ext/bc/qVnn172mjbEzRtCqqxyWhQwJ7jNX3dTjRyNjPTLr4x8zoLj6J"
 	networkID  = 1337
-	chainIDStr = "2mgSrj2xG9gagfhi5vSJwe6XiH2HmWinPxYCxhoZGSQzynRWPG"
+	chainIDStr = "qVnn172mjbEzRtCqqxyWhQwJ7jNX3dTjRyNjPTLr4x8zoLj6J"
 )
 
 type LightHeader struct {
@@ -44,12 +44,18 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
+	var validHeight uint64
+	first := true
 	go func() {
 		for {
 			log.Println("listening for light headers")
 			hght, dataRoot, rowRoots, colRoots, err := wsc.ListenLightHeaders(ctx, parser)
 			if err != nil {
 				log.Fatalln(err)
+			}
+			if first {
+				validHeight = hght
+				first = false
 			}
 			lhchan <- LightHeader{
 				Height:   hght,
@@ -60,51 +66,61 @@ func main() {
 		}
 	}()
 
+	go func() {
+		for {
+			lh := <-lhchan
+			if len(lh.RowRoots) != len(lh.ColRoots) {
+				log.Fatalln("invalid light header, height: ", lh.Height)
+			}
+			w := len(lh.RowRoots)
+			n := w * w
+			// sample 8 shares if number of shares are > 16.
+			// else sample half of the shares.
+			var s int
+			if n > 16 {
+				s = 8
+			} else {
+				s = n / 2
+			}
+			log.Printf("recevied light header for block %d, rows: %d\n", lh.Height, w)
+			rand.Seed(time.Now().UnixNano())
+			var k int         // Counter
+			var mu sync.Mutex // Mutex for safe access to k
+
+			var wg sync.WaitGroup // WaitGroup to wait for all goroutines to complete
+
+			for i := 0; i < s; i++ {
+				wg.Add(1) // Increment the WaitGroup counter
+				go func() {
+					defer wg.Done() // Decrement the WaitGroup counter when goroutine finishes
+					rowIDx := rand.Intn(w)
+					colIDx := rand.Intn(w)
+					log.Printf("fetching shares for row %d, column %d for block %d\n", rowIDx, colIDx, lh.Height)
+					proofs, err := cli.GetShareRowProofs(ctx, lh.Height, uint(rowIDx), uint(colIDx))
+					if err != nil {
+						panic(err)
+					}
+					if merkletree.VerifyProof(sha256.New(), lh.RowRoots[rowIDx], proofs, uint64(colIDx), uint64(w)) {
+						mu.Lock()
+						k++
+						mu.Unlock()
+					} else {
+						panic("can't verify")
+					}
+				}()
+			}
+
+			wg.Wait() // Wait for all sampling goroutines to complete
+			if k == s {
+				validHeight = lh.Height
+			}
+			log.Printf("Total successful samples for block %d: %d of %d. updated valid height to %d\n", lh.Height, k, s, lh.Height)
+		}
+	}()
+
 	for {
-		lh := <-lhchan
-		if len(lh.RowRoots) != len(lh.ColRoots) {
-			log.Fatalln("invalid light header, height: ", lh.Height)
-		}
-		w := len(lh.RowRoots)
-		n := w * w
-		// sample 8 shares if number of shares are > 16.
-		// else sample half of the shares.
-		var s int
-		if n > 16 {
-			s = 8
-		} else {
-			s = n / 2
-		}
-		log.Printf("recevied light header for block %d, rows: %d\n", lh.Height, w)
-		rand.Seed(time.Now().UnixNano())
-		var k int         // Counter
-		var mu sync.Mutex // Mutex for safe access to k
-
-		var wg sync.WaitGroup // WaitGroup to wait for all goroutines to complete
-
-		for i := 0; i < s; i++ {
-			wg.Add(1) // Increment the WaitGroup counter
-			go func() {
-				defer wg.Done() // Decrement the WaitGroup counter when goroutine finishes
-				rowIDx := rand.Intn(w)
-				colIDx := rand.Intn(w)
-				log.Printf("fetching shares for row %d, column %d for block %d\n", rowIDx, colIDx, lh.Height)
-				proofs, err := cli.GetShareRowProofs(ctx, lh.Height, uint(rowIDx), uint(colIDx))
-				if err != nil {
-					panic(err)
-				}
-				if merkletree.VerifyProof(sha256.New(), lh.RowRoots[rowIDx], proofs, uint64(colIDx), uint64(w)) {
-					mu.Lock()
-					k++
-					mu.Unlock()
-				} else {
-					panic("can't verify")
-				}
-			}()
-		}
-
-		wg.Wait() // Wait for all sampling goroutines to complete
-		log.Printf("Total successful samples for block %d: %d of %d\n", lh.Height, k, s)
+		log.Printf("current valid height: %d\n", validHeight)
+		time.Sleep(10 * time.Second)
 	}
 
 }
